7c8a8e299f85a1a9c13f6995fedcdd06f169c929
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-07-03 12:13:54 +0200

    icon added button deleted

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
-let buttonOne = document.querySelector(".buttonOne");
 let tlo = document.querySelector(".background");
 let buttonTwo = document.querySelector(".buttonThemelight");
 let lightdarkicon = document.querySelector(".lightModeicon");
 
-buttonOne.addEventListener("click", () => {
-    //W celu ćwiczenia zmiana tekstu w przycisku
-    buttonOne.innerText = buttonOne.innerText === "Light Mode" ? "Dark Mode" : "Light Mode"
-});
-buttonTwo.addEventListener("click", () => {
+
+    buttonTwo.addEventListener("click", () => {
     buttonTwo.classList.toggle("buttonThemedark");
     tlo.classList.toggle("dark");
     document.getElementById("light").src = tlo.classList.contains("dark") ? "images/night-mode.png" : "images/brightness.png"
