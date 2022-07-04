2c389b4ddf03e710d4243cc35591f31258da5841
Pawel Leszczynski <pawelleszczynski97@gmail.com>
2022-07-04 20:23:17 +0200

    functions added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-const buttonThemeColor = document.querySelector(".js-mainSection__buttonThemecontainer__buttonLightmode");
-buttonThemeColor.addEventListener("click", () => {
-    const backgroundColor = document.querySelector(".js-backgroundTheme");
-    buttonThemeColor.classList.toggle("mainSection__buttonThemecontainer__buttonDarkmode");
-    backgroundColor.classList.toggle("darkTheme");
-    document.getElementById("themeChanger").src = backgroundColor.classList.contains("darkTheme") ? "images/night-mode.png" : "images/brightness.png"
-});
-
+{
+    const changeButtonTheme = document.querySelector(".js-mainSection__buttonThemecontainer__buttonLightmode");
+    changeButtonTheme.addEventListener("click", () => {
+        changeButtonTheme.classList.toggle("mainSection__buttonThemecontainer__buttonDarkmode");
+        backgroundTheme();
+        buttonThemeImageChange();
+    });
+    const backgroundTheme = () => {
+        const backgroundColor = document.querySelector(".js-backgroundTheme");
+        backgroundColor.classList.toggle("darkTheme");
+    }
+    const buttonThemeImageChange = () => {
+        const backgroundColor = document.querySelector(".js-backgroundTheme");
+        document.getElementById("themeChanger").src = backgroundColor.classList.contains("darkTheme") ? "images/night-mode.png" : "images/brightness.png"
+    }
+}
